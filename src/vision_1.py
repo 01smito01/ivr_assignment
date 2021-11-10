e064151c1c4dd2f6a6fa448ed9fa03a0825d7d25
Keara <s1967114@ed.ac.uk>
2021-11-10 14:26:47 +0000

    Created vision_1.py file

---
+++ src/vision_1.py
@@ -0,0 +1,7 @@
+# solution for Section 2.1. This file should initiate a node that subscribes to image topics:
+# ”/camera1/robot/image raw”
+# ”/camera2/robot/image raw”
+# and publishes the 3 joint state topics:
+# ”joint angle 2”
+# ”joint angle 3”
+# ”joint angle 4”
