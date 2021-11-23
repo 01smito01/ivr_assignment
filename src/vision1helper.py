22bb43a97bba701d66f61911c5e86fbe217f5cc4
Keara <s1967114@ed.ac.uk>
2021-11-23 15:15:23 +0000

    fixed and gave publishing functionality

--- src/vision1helper.py
+++ src/vision1helper.py
@@ -8,31 +8,46 @@ import cv2
 import numpy as np
 from std_msgs.msg import Float64MultiArray, Float64
 
-def signal_publisher():
+def v1signal_publisher():
 
     #initialise node
     rospy.init_node('signal_publisher', anonymous=True)
     rate = rospy.Rate(50)
 
-    #get necessary values, constants
-    t0 = rospy.get_time()
-    curr_time = np.array([rospy.get_time()]) - t0
-    pi = np.pi
-
-    #calculate joint positions
-    joint_2 = pi/2 * np.sin(curr_time * pi/15)
-    joint_3 = pi/2 * np.sin(curr_time * pi/20)
-    joint_4 = pi/2 * np.sin(curr_time * pi/18)
-
-    #publish
+    #initialise publishers
     joint_2_pub = rospy.Publisher("/robot/joint2_position_controller/command", Float64, queue_size=10)
     joint_3_pub = rospy.Publisher("/robot/joint3_position_controller/command", Float64, queue_size=10)
     joint_4_pub = rospy.Publisher("/robot/joint4_position_controller/command", Float64, queue_size=10)
 
+    #get necessary values, constants
+    t0 = rospy.get_time()
+
+    while not rospy.is_shutdown():
+        curr_time = np.array([rospy.get_time()]) - t0
+        pi = np.pi
+        #calculate joint positions
+        joint_2 = pi/2 * np.sin(curr_time * pi/15)
+        joint_3 = pi/2 * np.sin(curr_time * pi/20)
+        joint_4 = pi/2 * np.sin(curr_time * pi/18)
+
+        targetj2 = Float64
+        targetj3 = Float64
+        targetj4 = Float64
+
+        targetj2.data = joint_2
+        targetj3.data = joint_3
+        targetj4.data = joint_4
+
+        joint_2_pub.publish(targetj2)
+        joint_3_pub.publish(targetj3)
+        joint_4_pub.publish(targetj4)
+
+
+
 #run code if node is called
 if __name__ == '__main__':
     try:
-        signal_publisher()
+        v1signal_publisher()
     except rospy.ROSInterruptException:
         pass
 
