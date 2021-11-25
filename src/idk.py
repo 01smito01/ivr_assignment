9898cd296230859f61e36e7e2d7a9d2806e0169d
Oliver Smith <01smito01@gmail.com>
2021-11-25 23:32:04 +0000

    Smarter occlusion code

--- src/idk.py
+++ src/idk.py
@@ -10,6 +10,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float64MultiArray, Float64
 from cv_bridge import CvBridge, CvBridgeError
+pi = np.pi
 
 
 class image_converter:
@@ -18,14 +19,13 @@ class image_converter:
     def __init__(self):
         # initialize the node named image_processing
         rospy.init_node('image_processing', anonymous=True)
-        r = rospy.Rate(100)
+        r = rospy.Rate(100)  # attempt to stop the 1st callback being used like 3x as often as the 2nd
 
         # subscriptions
         self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw", Image, self.callback1)
         self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw", Image, self.callback2)
 
         # publishers
-
         self.robot_joint2_pub = rospy.Publisher("joint_angle_2", Float64, queue_size=10)
         self.robot_joint3_pub = rospy.Publisher("joint_angle_3", Float64, queue_size=10)
         self.robot_joint4_pub = rospy.Publisher("joint_angle_4", Float64, queue_size=10)
@@ -41,44 +41,52 @@ class image_converter:
         self.pixel_to_meter = 4 / ((535 - 432) ** 2) ** 0.5  # kind of assumes this is equal for each camera
 
     # Detection code mostly copied from the labs, w/ some additional smartness when we can't see blobs
-    def detect_red(self, image):
+    def detect_red(self, image, caller):
         mask = cv2.inRange(image, (0, 0, 100), (0, 0, 255))
         kernel = np.ones((5, 5), np.uint8)
         mask = cv2.dilate(mask, kernel, iterations=3)
         M = cv2.moments(mask)
-        if (M['m00'] != 0.0):
+        if M['m00'] != 0.0:  # check blob is visible
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
         else:  # ASSUMPTION: if you can't see the blob, pretend it's where you last saw it
-            cx = 0
-            cy = 0
+            if caller == 1:
+                cx = self.red[1]
+            else:
+                cx = self.red[0]
+            cy = self.red[2]
         return np.array([cx, cy])
 
-    def detect_blue(self, image):
+    def detect_blue(self, image, caller):
         mask = cv2.inRange(image, (100, 0, 0), (255, 0, 0))
         kernel = np.ones((5, 5), np.uint8)
         mask = cv2.dilate(mask, kernel, iterations=3)
         M = cv2.moments(mask)
-        if (M['m00'] != 0.0):
+        if M['m00'] != 0.0:
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
         else:
-            cx = 0
-            cy = 0
+            if caller == 1:
+                cx = self.blue[1]
+            else:
+                cx = self.blue[0]
+            cy = self.blue[2]
         return np.array([cx, cy])
 
-    def detect_joint_angles(self, image):
+    def detect_joint_angles(self, image, caller):
         a = self.pixel_to_meter
         center = a * self.green
         c1Pos = a * self.yellow
-        c2Pos = a * self.detect_blue(image)
-        c3Pos = a * self.detect_red(image)
+        c2Pos = a * self.detect_blue(image, caller)
+        c3Pos = a * self.detect_red(image, caller)
 
         ja1 = np.arctan2(center[0] - c1Pos[0], center[1] - c1Pos[1])
         ja2 = np.arctan2(c1Pos[0] - c2Pos[0], c1Pos[1] - c2Pos[1]) - ja1
         ja3 = np.arctan2(c2Pos[0] - c3Pos[0], c2Pos[1] - c3Pos[1]) - ja2 - ja1
 
         return np.array([ja1, ja2, ja3])
+
+    # coords from our cameras
     def get_xz(self, blob, detector):
         temp = blob
         detection = detector
@@ -93,6 +101,7 @@ class image_converter:
         temp[2] = detection[1]
         return np.array(temp)
 
+    # takes 3 linked points and spits out the acutest angle between them
     def vec_angle(self, base, end_a, end_b):
         v1 = end_a - base
         v2 = end_b - base
@@ -115,11 +124,12 @@ class image_converter:
 
     def joint3side(self):
         angle = self.vec_angle(self.yellow, self.green, self.blue)
-        print(self.blue[1])
-        if self.blue[1] > 400:
-            return angle - math.pi
-        else:
-            return -angle + math.pi
+        if angle > pi/2:
+            angle = pi - angle
+        elif angle < -pi/2:
+            angle = angle - pi
+        return angle
+
 
     # Recieve data from camera 1, process it, and publish
     def callback1(self, data):
@@ -129,10 +139,10 @@ class image_converter:
         except CvBridgeError as e:
             print(e)
 
-        self.red = self.get_yz(self.red, self.detect_red(self.cv_image1))
-        self.blue = self.get_yz(self.blue, self.detect_blue(self.cv_image1))
+        self.red = self.get_yz(self.red, self.detect_red(self.cv_image1, 1))
+        self.blue = self.get_yz(self.blue, self.detect_blue(self.cv_image1, 1))
 
-        joint_data = self.detect_joint_angles(self.cv_image1)
+        joint_data = self.detect_joint_angles(self.cv_image1, 1)
 
         self.joints = Float64MultiArray()
         self.joints = joint_data
@@ -151,10 +161,10 @@ class image_converter:
         im2 = cv2.imshow('Camera 2', self.cv_image2)
         cv2.waitKey(3)
 
-        joint_data = self.detect_joint_angles(self.cv_image2)
+        joint_data = self.detect_joint_angles(self.cv_image2, 2)
 
-        self.red = self.get_xz(self.red, self.detect_red(self.cv_image2))
-        self.blue = self.get_xz(self.blue, self.detect_blue(self.cv_image2))
+        self.red = self.get_xz(self.red, self.detect_red(self.cv_image2, 2))
+        self.blue = self.get_xz(self.blue, self.detect_blue(self.cv_image2, 2))
 
         self.joints = Float64MultiArray()
         self.joints = joint_data
